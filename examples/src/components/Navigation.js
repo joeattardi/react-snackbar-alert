b8e3ffce55a10267070971a74ab3171580f9f02f
Joe Attardi <joe@attardi.net>
2019-05-28 23:45:45 -0400

    fix active link issue

--- examples/src/components/Navigation.js
+++ examples/src/components/Navigation.js
@@ -8,17 +8,17 @@ export default function Navigation() {
     <nav id={styles.sidebar}>
       <ul>
         <li><Link to="/" activeClassName={styles.active}>Home</Link></li>
-        <li><Link to="/gettingStarted" activeClassName={styles.active}>Getting Started</Link></li>
-        <li><Link to="/api" activeClassName={styles.active}>API</Link></li>
+        <li><Link to="/gettingStarted/" activeClassName={styles.active}>Getting Started</Link></li>
+        <li><Link to="/api/" activeClassName={styles.active}>API</Link></li>
       </ul>
       <h2>Examples</h2>
       <ul>
-        <li><Link to="/examples/base" activeClassName={styles.active}>Base</Link></li>
-        <li><Link to="/examples/customTimeout" activeClassName={styles.active}>Custom Timeout</Link></li>
-        <li><Link to="/examples/customAnimationTimeout" activeClassName={styles.active}>Custom Animation Timeout</Link></li>
-        <li><Link to="/examples/customComponent" activeClassName={styles.active}>Custom Snackbar Component</Link></li>
-        <li><Link to="/examples/customChildComponent" activeClassName={styles.active}>Extending Snackbar Component</Link></li>
-        <li><Link to="/examples/customComponentData" activeClassName={styles.active}>Custom Component Data</Link></li>
+        <li><Link to="/examples/base/" activeClassName={styles.active}>Base</Link></li>
+        <li><Link to="/examples/customTimeout/" activeClassName={styles.active}>Custom Timeout</Link></li>
+        <li><Link to="/examples/customAnimationTimeout/" activeClassName={styles.active}>Custom Animation Timeout</Link></li>
+        <li><Link to="/examples/customComponent/" activeClassName={styles.active}>Custom Snackbar Component</Link></li>
+        <li><Link to="/examples/customChildComponent/" activeClassName={styles.active}>Extending Snackbar Component</Link></li>
+        <li><Link to="/examples/customComponentData/" activeClassName={styles.active}>Custom Component Data</Link></li>
       </ul>
     </nav>
   );
